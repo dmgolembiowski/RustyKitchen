418568a25abc3678fc7b4dc163fc273b138e23e4
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-07 00:55:11 -0400

    Veeery cheeky

--- Enumerations/option1/src/main.rs
+++ Enumerations/option1/src/main.rs
@@ -1,3 +1,18 @@
+// Lines 2-5 happen automatically in the prelude!
+enum _Option<T> {
+    Some(T),        // Option::Some
+    None,           // Option::None <-- Rust's null value
+}
+
 fn main() {
-    println!("Hello, world!");
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+    /* Note:
+     * -----
+     *
+     * If using `None`, need to tell Rust compiler
+     * which generic type of None it is covering for,
+     * b/c otherwise it cannot properly infer the type.
+     */
 }
