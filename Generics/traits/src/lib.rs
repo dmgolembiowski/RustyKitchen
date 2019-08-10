794c11b5e0bbc119725caaf38fad10855d7b136d
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-10 15:47:01 -0400

    Check readme for next steps

--- Generics/traits/src/lib.rs
+++ Generics/traits/src/lib.rs
@@ -2,10 +2,46 @@ pub trait Summary {
     fn summarize(&self) -> String;
 }
 
+mod twitter {
+   pub struct NewsArticle {
+        pub headline: String,
+        pub location: String,
+        pub author: String,
+        pub content: String,
+    }
+
+    impl Summary for NewsArticle {
+        fn summarize(&self) -> String {
+            format!("{}, by {} ({})",
+                self.headline,
+                self.author,
+                self.location)
+        }
+    }
+
+    pub struct Tweet {
+        pub username: String,
+        pub content: String,
+        pub reply: bool,
+        pub retweet: bool,
+    }
+
+    impl super::Summary for Tweet {
+        fn summarize(&self) -> String {
+            format!("{}: {}", self.username, self.content)
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let tweet = super::twitter::Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know"),
+            reply: false,
+            retweet: false,
+        };
+        println!("1 new tweet: {}", tweet.summarize());
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # RustyKitchen
 This might be my new "thing".
 
-Resume reading on page 210/618 https://dgolembiowski.com/Academia/Books/Programming/The_Rust_Programming_Language.pdf The_Rust_Programming_Language.pdf
+Resume reading on page 217/618 https://dgolembiowski.com/Academia/Books/Programming/The_Rust_Programming_Language.pdf The_Rust_Programming_Language.pdf
+
+To do:
+Get the implementation for Generics/.../src/lib.rs
+working so that the `pub trait Summary` can be used on the 
+twitter module in src/lib.rs
