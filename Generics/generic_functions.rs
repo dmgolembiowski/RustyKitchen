f1c5f3c5888279a4ad2fdeed09663341dab98478
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-10 03:55:19 -0400

    It seems that when implementing comparison operations, which might not work out of the box for all types, it's useful to define std::cmp::PartialOrd traits, which resemble __gt__, __lt__, __eq__, etc. in Python. In Rust, these traits are defined at the Program level. Commonly, they'll be present in Struct defn's and others too.

--- Generics/generic_functions.rs
+++ Generics/generic_functions.rs
@@ -4,5 +4,21 @@
  * parameter named `list`, which is a slice
  * of values of type T. The largest function
  * will return a value of the same type T. */
-fn largest<T>(list: &[T]) -> T {}
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 // Finally, this can be more convenient
+fn main() {
+    let nums = vec![1,2,3,4,432,4];
+    let result = largest(&nums);
+    println!("The largest number is {}", result);
+    let char_list = vec!['y','m','a','q'];
+    let result = largest(&char_list);
+    println!("{}", result);
+}
