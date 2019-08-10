c0d9b067b11ac303d5bdb89edce6334d636e9b97
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-09 23:10:01 -0400

    Added more recipes

---
+++ Experimenting/Command_Line/arguments/src/print_bold_term.rs
@@ -0,0 +1,8 @@
+extern crate ansi_term;
+
+use ansi_term::Style;
+
+fn main() {
+    println!("{} and this is not",
+             Style::new().bold().paint("This is Bold"));
+}
---
+++ Experimenting/Command_Line/compression/src/compress_dir_to_tarball.rs
@@ -0,0 +1,14 @@
+extern crate tar;
+extern crate flate2;
+
+use std::fs::File;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+
+fn main() -> Result<(), std::io::Error> {
+    let tar_gz = File::create("archive.tar.gz")?;
+    let enc = GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = tar::Builder::new(enc);
+    tar.append_dir_all("backup/logs", "/var/log")?;
+    Ok(())
+}
