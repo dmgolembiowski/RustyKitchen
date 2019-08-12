c057eebeafb6ba08c2f380ff89071c057b0469a4
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-12 14:29:22 -0400

    A ".expect()" call can be replaced with a "?" operator, which rather than using a "panic!" on an error, "?" returns the error value from the current function for the caller to handle.

--- Experimenting/minigrep/src/main.rs
+++ Experimenting/minigrep/src/main.rs
@@ -1,26 +1,45 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 struct Configuration {
     query: String,
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Configuration {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Configuration {
+    fn new(args: &[String]) -> Result<Configuration, &'static str> {
+        if args.len() < 3 {
+            return Err("\n \
+                Suggested usage: cargo run [string] [file location] \
+            \n");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
     
-    Configuration { query, filename }
+        Ok(Configuration { query, filename })
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
-    println!("Searching for {}", &config.query);
+    let config = Configuration::new(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
+    println!("Searching for {}", &config.query);
     println!("In file: {}", &config.filename);
 
-    let contents = fs::read_to_string(&config.filename)
-        .expect("Something went wrong reading the file");
-    println!("With text:\n{}", &contents);
+    run(config);
+
+}
+
+fn run(config: Configuration) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+    Ok(())
 }
