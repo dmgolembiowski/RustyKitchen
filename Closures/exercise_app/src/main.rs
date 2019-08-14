5155eea24b68de1de79696cb625b5a68f73be647
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-14 14:34:55 -0400

    closures

--- Closures/exercise_app/src/main.rs
+++ Closures/exercise_app/src/main.rs
@@ -1,13 +1,31 @@
+extern crate rand;
+
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
+fn simulated_expensive_calculation() -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
-    intensity
+    rand::random::<u32>()
 }
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    if intensity < 25 {
+        println!("{}", simulated_expensive_calculation(intensity));
+        println!("{}", simulated_expensive_calculation(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today!");
+        } else {
+            println!("{}", simulated_expensive_calculation(intensity));
+        }
+    }
+
+}
+
 fn main() {
-    let input: u32 = 8;
-    let _intensity: u32 = simulated_expensive_calculation(input);
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = rand::random::<u32>();
+    let _intensity: u32 = simulated_expensive_calculation();
     println!("Done!");
 }
