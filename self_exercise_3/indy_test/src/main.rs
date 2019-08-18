6b177d4345e84c00c9eb604043b20c7cbadee55c
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-18 17:14:28 -0400

    The base struct and impl for the Cache is now shown to work, but now it needs to privately set the updated key-value pair every time it is updated.

--- self_exercise_3/indy_test/src/main.rs
+++ self_exercise_3/indy_test/src/main.rs
@@ -8,11 +8,23 @@ struct Cache<'a, T>
     internal: HashMap<&'a u32, &'a u32>
 }
 
-impl<'a, T> Cacher<'a, T>
+#[allow(dead_code)]
+impl<'a, T> Cache<'a, T>
     where T: Fn(u32) -> u32
 {
-    fn new(calculation: T) -> Cacher<'a, T> {
-        
+    fn new(calculation: T) -> Cache<'a, T> {
+        Cache {
+            calculation,
+            internal: HashMap::new(),
+        }
+    }
+
+    fn set(&mut self, arg: &'a u32, value: &'a u32) {
+        self.internal.insert(arg, value);
+    }
+
+    fn get(&mut self, arg: &'a u32) -> &'a u32 {
+        self.internal[arg]
     }
 }
 
