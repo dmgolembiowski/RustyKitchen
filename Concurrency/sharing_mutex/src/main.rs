07dd8cdf8ec31f9c82bbfc832f3d5e3c0746109d
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-27 15:32:31 -0400

    Once the chapter is complete, try to create a program in Rust that has a deadlock (when an operation needs to lock two resources and two threads have each acquired one of the locks, causing them to wait for each other forever), then research deadlock mitigation strategies for mutexes in some language other than Python, and have a go at implementing them in Rust. Note that the standard library API documentation for 'Mutex<T>' and 'MutexGuard<T>' provide helpful info.

--- Concurrency/sharing_mutex/src/main.rs
+++ Concurrency/sharing_mutex/src/main.rs
@@ -2,18 +2,22 @@ use std::sync::{Mutex,Arc};
 use std::thread;
 //use std::rc::Rc;
 
+/// Gonna use Arc, which from the book sounds dangerously similar
+/// to a "semaphore" paired with a pointer
 fn main() {
-    // Gonna use Arc, which from the book sounds dangerously similar
-    // to a "semaphore" paired with a pointer
     //let counter = Rc::new(Mutex::new(0));
-    let counter = Arc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0)); // using an Arc<T> to wrap
+                                           // the Mutex<T> to be able to
+                                           // share ownership across
+                                           // multiple threads
     let mut handles = vec![];
 
     for _ in 0..10 {
         let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
-            //let mut num = counter.lock().unwrap();
-            //*num += 1
+            /* let mut num = counter.lock().unwrap();
+             * *num += 1
+             */
             *&mut *counter.lock().unwrap() += 1; // <- This cheeky one liner
         });
         handles.push(handle);
