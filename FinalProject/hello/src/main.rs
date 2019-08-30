9a4fd96d55f68a9642d9023fed356e056b1d609b
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-30 16:33:04 -0400

    The 'lossy' part of the name indicates the behavior of this function when it sees an invalid UTF-8 sequence: it will replace the invalid sequence with U+FFFD REPLACEMENT CHARACTER.

--- FinalProject/hello/src/main.rs
+++ FinalProject/hello/src/main.rs
@@ -1,17 +1,21 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    // .bind() returns a `Result<T, E>` indicating that the binding
-    // could fail. Note: binding wouldn't work if we ran two instances 
-    // of our program and so had two programs listening to the same port.
-
     for stream in listener.incoming() {
-
         let stream = stream.unwrap();
         // use .unwrap() to to stop the program if errors happen
-
-        println!("Connection est.");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
 
