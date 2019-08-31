a80bc30e4b43ad2adf34c53e5b53d4eb21c2d150
David Golembiowski <dmgolembiowski@gmail.com>
2019-08-30 21:28:14 -0400

    Need to now validate the number of Threads in `new`

---
+++ FinalProject/naive_examples/src/lib.rs
@@ -0,0 +1,19 @@
+pub struct ThreadPool;
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        ThreadPool
+    }
+    // Define the `execute` method on `ThreadPool` to take
+    // a closure as a parameter (as Fn, FnMut, or FnOnce)
+    
+    pub fn execute<F>(&self, f: F)
+        where
+        F: FnOnce() + Send + 'static
+        // Using `()` after `FnOnce` b/c this parameter 
+        // represents a closure that takes no parameters
+        // and doesn't return a value.
+        {
+            
+        }
+}
+
